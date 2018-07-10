1bd87bb5e122af0c773fc65cb44d81235ba83798
Larisa Botello <larisa.botello01@utrgv.edu>
2018-07-10 15:39:57 -0700

    Adding Completed App

---
+++ MagicEightBall/MagicEightBall/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Larisa Botello on 7/4/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var shakeButton: UIButton!
+    
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        generateAnswer()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func generateAnswer() {
+        let maxIndex = UInt32(answers.count)
+        let randomIndex = Int(arc4random_uniform(maxIndex))
+        
+        answerLabel.text = answers[randomIndex]
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        
+        generateAnswer()
+    }
+
+
+}
+
